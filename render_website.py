7bf001e7de944d8b58935260a6e0a0897f9a3a10
Nikita <39242713+Nais-Alkator@users.noreply.github.com>
2021-02-23 23:58:52 +0300

    The books were displayed in two columns

--- render_website.py
+++ render_website.py
@@ -1,22 +1,27 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json 
 from livereload import Server
+from more_itertools import chunked
 
-with open("json/info.json", "r", encoding='utf-8') as my_file:
-    books_summary = my_file.read()
 
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml']))
 
+
+
 def on_reload():
-	template = env.get_template('template.html')
-	rendered_page = template.render(
-		books=json.loads(books_summary)
-		)
-	with open('index.html', 'w', encoding="utf-8") as file:
-		    file.write(rendered_page)
+    with open("json/info.json", "r", encoding='utf-8') as my_file:
+      books_summary = my_file.read()
+    books_summary = json.loads(books_summary)
+    books_summary = list(chunked(books_summary, 2))
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        books_summary=books_summary
+        )
+    with open('index.html', 'w', encoding="utf-8") as file:
+            file.write(rendered_page)
 
 on_reload()
 
