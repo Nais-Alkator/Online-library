3673a6b001ccf7f997e5ea6edcbae0899021701b
Nikita <39242713+Nais-Alkator@users.noreply.github.com>
2021-03-10 23:36:10 +0300

    The books list splited by pages

--- render_website.py
+++ render_website.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json 
 from livereload import Server
 from more_itertools import chunked
-
+import os
 
 
 env = Environment(
@@ -15,13 +15,17 @@ def on_reload():
     with open("json/info.json", "r", encoding='utf-8') as my_file:
       books_summary = my_file.read()
     books_summary = json.loads(books_summary)
-    books_summary = list(chunked(books_summary, 2))
-    template = env.get_template('template.html')
-    rendered_page = template.render(
-        books_summary=books_summary
-        )
-    with open('index.html', 'w', encoding="utf-8") as file:
-            file.write(rendered_page)
+    books_summary = list(chunked(books_summary, 10))
+    for number, books_group in enumerate(books_summary):
+        books_subgroup = list(chunked(books_group, 2))
+        template = env.get_template('template.html')
+        rendered_page = template.render(
+            books_subgroup=books_subgroup
+            )
+        with open('pages/index{}.html'.format(number), 'w', encoding="utf-8") as file:
+                file.write(rendered_page)
+
+os.makedirs("pages", exist_ok=True)
 
 on_reload()
 
