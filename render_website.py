9f6dfd939a51f716523039c50e875451ab21bb06
Nikita <39242713+Nais-Alkator@users.noreply.github.com>
2021-06-05 20:21:02 +0300

    Two functions have been joined to one and the one template have been created.

--- render_website.py
+++ render_website.py
@@ -11,15 +11,16 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml']))
 
 
-def create_main_page():
+def create_pages():
+    #Создание главной страницы
     with open("json/info.json", "r", encoding='utf-8') as my_file:
-      books_summary = my_file.read()
-    books_summary = json.loads(books_summary)
+      books_json = my_file.read()
+    books_json = json.loads(books_json)
     books_per_page = 10
-    pages_quantity = (len(books_summary) / books_per_page)
+    pages_quantity = (len(books_json) / books_per_page)
     pages_quantity = math.ceil(pages_quantity)
-    books_summary = list(chunked(books_summary, 2))
-    template = env.get_template('main_template.html')
+    books_summary = list(chunked(books_json, 2))
+    template = env.get_template('template.html')
     rendered_page = template.render(
         books_summary=books_summary,
         pages_quantity=pages_quantity
@@ -27,23 +28,12 @@ def create_main_page():
     with open('index.html', 'w', encoding='utf-8') as file:
         file.write(rendered_page)
 
-
-def create_page():
-    with open("json/info.json", "r", encoding='utf-8') as my_file:
-      books_summary = my_file.read()
-    books_per_page = 10
-    books_summary = json.loads(books_summary)
-    books_summary = list(chunked(books_summary, books_per_page))
-    pages_quantity = len(books_summary)
+    #Создание дополнительных страниц
+    books_summary = list(chunked(books_json, books_per_page))
     page_number = 1
-
     for books_group in books_summary:
         books_subgroup = list(chunked(books_group, 2))
-        template = env.get_template('page_template.html')
-        previous_page_number = page_number - 1
-        previous_page_path = 'index{}.html'.format(previous_page_number)
-        next_page_number = page_number + 1
-        next_page_path = 'index{}.html'.format(next_page_number)
+        template = env.get_template('template.html')
         page_name = 'pages/index{}.html'.format(page_number)
         rendered_page = template.render(
             books_subgroup=books_subgroup,
@@ -56,13 +46,11 @@ def create_page():
 
 os.makedirs("pages", exist_ok=True)
 
-create_main_page()
-
-create_page()
+create_pages()
 
 server = Server()
 
-server.watch('main_template.html', create_main_page)
+server.watch('main_template.html', create_pages)
 
 server.serve(root='.')
 
